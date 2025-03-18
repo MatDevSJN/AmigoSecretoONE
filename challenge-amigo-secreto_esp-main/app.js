cdc9d717acd9418ce134ce2cf6cd3f5dd6e5f98e
MatDevSJN <helloworld.villanuevam@gmail.com>
2025-03-18 15:19:12 -0300

    correccion html y avance en app js

--- challenge-amigo-secreto_esp-main/app.js
+++ challenge-amigo-secreto_esp-main/app.js
@@ -1,22 +1,15 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-let amigos = [];
-
-function agregarAmigo() {
-    const input = document.getElementById("amigo");
-    const nombre = input.value.trim();
-    if (nombre !== "") {
-        amigos.push(nombre);
-        actualizarLista();
-        input.value = "";
-    }
+function sortearAmigo() {
+    let resultado = amigos.map(amigo => ${amigo} → ${amigos[Math.floor(Math.random() * amigos.length)]});
+    mostrarResultado(resultado);
 }
 
-function actualizarLista() {
-    const lista = document.getElementById("listaAmigos");
-    lista.innerHTML = "";
-    amigos.forEach(amigo => {
+function mostrarResultado(resultado) {
+    const resultadoLista = document.getElementById("resultado");
+    resultadoLista.innerHTML = "";
+    resultado.forEach(pair => {
         const li = document.createElement("li");
-        li.textContent = amigo;
-        lista.appendChild(li);
+        li.textContent = pair;
+        resultadoLista.appendChild(li);
     });
 }
