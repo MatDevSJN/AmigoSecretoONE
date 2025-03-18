742b0f92d297a2a0cb3abbfc7dc8b517fb845171
MatDevSJN <helloworld.villanuevam@gmail.com>
2025-03-18 15:01:58 -0300

    primera parte para ver

--- challenge-amigo-secreto_esp-main/app.js
+++ challenge-amigo-secreto_esp-main/app.js
@@ -1,1 +1,22 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo() {
+    const input = document.getElementById("amigo");
+    const nombre = input.value.trim();
+    if (nombre !== "") {
+        amigos.push(nombre);
+        actualizarLista();
+        input.value = "";
+    }
+}
+
+function actualizarLista() {
+    const lista = document.getElementById("listaAmigos");
+    lista.innerHTML = "";
+    amigos.forEach(amigo => {
+        const li = document.createElement("li");
+        li.textContent = amigo;
+        lista.appendChild(li);
+    });
+}
